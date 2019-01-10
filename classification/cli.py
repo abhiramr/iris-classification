4f8a3d117ea1898bbbd4f7ae218fb6758ced7285
Abhiram R <abhi.darkness@gmail.com>
2019-01-10 15:44:25 +0530

    Added classification project and setup files

---
+++ classification/cli.py
@@ -0,0 +1,39 @@
+"""
+Hallmark Command Line 
+------------------
+
+"""
+import os
+import sys
+from texttable import Texttable
+import click
+
+from . import spec 
+
+@click.group()
+def main():
+    """ 
+    Commandline for Iris Specifications 
+    """
+    pass
+
+
+@main.group("schema")
+def schema():
+    """
+    Discovery and operation specification formats
+    """
+    pass 
+
+
+@schema.command("list")
+def _schema_list():
+    """
+    List available schemas 
+    """
+
+    parser = spec.parser() 
+    summary = parser.schema_list()
+    table = Texttable()
+    table.add_rows(summary)
+    print(table.draw())
